b9d059a92262caccfe0cd589a9fbb7df55153a8e
Pedro Rodrigues <pmartinhorodrigues@gmail.com>
2023-03-28 16:45:06 -0300

    att

---
+++ Exercícios/Modulo Extra/iphone/scripts.js
@@ -0,0 +1,9 @@
+function trocaCor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaIphone(imagem){
+    let iphone = document.querySelector(".iphone")
+    iphone.src = imagem
+}
